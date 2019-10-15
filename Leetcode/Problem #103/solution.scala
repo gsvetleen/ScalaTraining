0fc2208cadcac13606cc40ada60fe0cd97e987e4
Benjamin Garrett <bgarrett@temple.edu>
2019-10-15 14:34:29 -0400

    Added a more idiomatic version that's completely functional, albeit probably with worse run time performance, although Leetcode says it's the best performing version from a memory consumption standpoint

--- Leetcode/Problem #103/solution.scala
+++ Leetcode/Problem #103/solution.scala
@@ -40,3 +40,33 @@ object Solution {
   }
 }
 // Given binary tree [3,9,20,null,null,15,7]
+
+// Benjamin's version
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(var _value: Int) {
+ *   var value: Int = _value
+ *   var left: TreeNode = null
+ *   var right: TreeNode = null
+ * }
+ */
+object Solution {
+    def zigzagLevelOrder(root: TreeNode): List[List[Int]] = {
+        def get(node: TreeNode): List[Int] = if(node==null) List() else List(node.value)
+        def sweep(node: TreeNode, level: Int, depth: Int): List[Int] = {
+            if(node==null) return Nil
+            if(level%2==0)
+                if(depth==level-1) return List(get(node.left),get(node.right)).flatten
+                else return sweep(node.left,level,depth+1) ::: sweep(node.right,level,depth+1)
+            else
+                if(depth==level-1) return List(get(node.right),get(node.left)).flatten
+                else return sweep(node.right,level,depth+1) ::: sweep(node.left,level,depth+1)
+        }
+        def all(root: TreeNode, level: Int, result: List[List[Int]]): List[List[Int]] = {
+            val r = sweep(root,level,0)
+            if(r==Nil) result
+            else all(root,level+1,r :: result)
+        }
+        if(root!=null) all(root,1,List(List(root.value))).reverse else List()
+    }
+}
