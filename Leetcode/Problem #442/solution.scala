039a4908b16ab976ce9d76d0767aed22b2bbf5e9
Benjamin Garrett <bgarrett@temple.edu>
2019-10-15 23:47:34 -0400

    Added fast and low memory solution

--- Leetcode/Problem #442/solution.scala
+++ Leetcode/Problem #442/solution.scala
@@ -18,3 +18,18 @@ object Solution {
 //Example: Input: [4,3,2,7,8,2,3,1]
 //
 //
+
+//Runtime: 5168 ms, faster than 91.67% of Scala online submissions for Find All Duplicates in an Array.
+//Memory Usage: 68.6 MB, less than 100.00% of Scala online submissions for Find All Duplicates in an Array.
+object Solution {
+    def findDuplicates(nums: Array[Int]): List[Int] = {
+        def f(a:Array[Int],j:Int,n:Int,r:List[Int]):List[Int]={
+            if(j==n) r
+            else if(a(j)>1) f(a,j+1,n,j+1::r)
+            else f(a,j+1,n,r)
+        }
+        var r=Array.ofDim[Int](nums.length)
+        nums.map(x=>r(x-1)+=1)
+        f(r,0,nums.length,Nil)
+    }
+}
